30f653448d6dbfabdcdbba0ccb4206e1378ce47b
Olga <olgaalaquas@gmail.com>
2024-08-26 12:22:56 +0200

    initial project

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+import Hero from './Hero';
+import ImagesGallery from './ImagesGallery';
+
+
+function App() {
+  return (
+    <>
+    <Header><h1>PILATES con P de "Puedes con todo"</h1></Header>
+      
+    </>
+  );
+}
+
+export default App;
