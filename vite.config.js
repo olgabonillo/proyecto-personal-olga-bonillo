2d14ba0a5757e61b4cebd1ad7def8c3775ef2c48
Olga <olgaalaquas@gmail.com>
2024-09-18 16:57:14 +0200

    add base url

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "./docs",
+    emptyOutDir: true,
+  },
+  base: "/proyecto-personal-olga-bonillo/"
 })
